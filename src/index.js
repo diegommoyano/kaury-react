dbf16c09ea2ca5139d503c0d8b59816c7545d016
Diego Moyano <diegommoyano@gmail.com>
2020-05-29 08:59:11 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { AlertaCerrable as alertaCerrable } from './components/AlertaCerrable';
 import { ConfirmacionDialog as confirmacionDialog } from './components/ConfirmacionDialog';
 import { CustomFilterText as customFilterText } from './components/CustomFilterText';
-export FormSelect from './FormSelect';
+export { FormSelect } from './FormSelect';
 import { LoginForm as loginForm } from './components/LoginForm';
 import { NavBar as navBar } from './components/NavBar';
 import { PrivateRoute as privateRoute } from './components/PrivateRoute';
