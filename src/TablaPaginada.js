a1e28563a4a174bd27122ba039dfc33e4ea42b1f
Diego Moyano <diegommoyano@gmail.com>
2020-06-10 23:33:50 -0300

    .

--- src/TablaPaginada.js
+++ src/TablaPaginada.js
@@ -55,8 +55,8 @@ const useStyles = makeStyles(theme => ({
 
 const StyledTableCell = withStyles(theme => ({
   head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
+    //backgroundColor: theme.palette.common.black,
+    //color: theme.palette.common.white
   },
   body: {
     fontSize: 14
