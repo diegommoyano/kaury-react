86cb172a348132214bb410bb38c9df67941a0480
Diego Moyano <diegommoyano@gmail.com>
2020-10-12 19:49:48 -0300

    .

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export { NavBar  } from './NavBar';
 export { PrivateRoute } from './PrivateRoute';
 export { SelectMarca  } from './SelectMarca';
 export { SelectUnidadDeMedida  } from './SelectUnidadDeMedida';
-export { TablaPaginada, Columna, SortOrder, ASC, DESC} from './TablaPaginada';
+export { TablaPaginada, Columna, SortOrder, COLUMN_COMPONENT, COLUMN_TEXT, ASC, DESC} from './TablaPaginada';
 export { Tabla } from './Tabla'
 export { useSearchParams  } from './hooks'
 
